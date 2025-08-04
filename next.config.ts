b3812edc8376db2f985ac46f5c0255ec67796e6b
William Kim <wik002@ucsd.edu>
2025-08-04 12:47:20 -0700

    enabled output config for deployment

--- next.config.ts
+++ next.config.ts
@@ -2,7 +2,7 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
-  // output: 'export', // this will create and out folder for the application. Comment this out while developing locally
+  output: 'export', // this will create and out folder for the application. Comment this out while developing locally
 };
 
 export default nextConfig;
