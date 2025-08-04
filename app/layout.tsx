76ccfb4754d64a9129534f21cb4cf5dcfdc9cfed
William Kim <wik002@ucsd.edu>
2025-08-04 13:02:21 -0700

    changing configs to target vercel deployment

--- app/layout.tsx
+++ app/layout.tsx
@@ -10,9 +10,9 @@ const defaultUrl = process.env.VERCEL_URL
   : "http://localhost:3000";
 
 export const metadata: Metadata = {
-  // metadataBase: new URL(defaultUrl),
-  title: "Next.js and Supabase Starter Kit",
-  description: "The fastest way to build apps with Next.js and Supabase",
+  metadataBase: new URL(defaultUrl),
+  title: "Stacked AI Habit Tracker",
+  description: "The easiest way to create and manage your habits, with a personal LLM assisstant.",
 };
 
 const geistSans = Geist({
